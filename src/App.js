dcf2b7b30dc60c00fea6ad8079b3c94f92eff33e
Mouhcine Slimani <muhcine.slimani@gmail.com>
2023-11-05 01:21:45 +0100

    install sass && configuration app to be deployed in github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import './css/app.scss';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        Bonjour tout le monde
+      </header>
     </div>
   );
 }
